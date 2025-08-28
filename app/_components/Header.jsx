46c9fb7d9d68c7f559c7ba6a54bbfc070ae4134b
AmanPurohit73 <amanpurohit315@gmail.com>
2025-08-28 16:55:21 +0530

    auth done

---
+++ app/_components/Header.jsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { UserButton, useUser } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  const { user, isSignedIn } = useUser();
+
+  return (
+    <div className="p-5 flex justify-between items-center border shadow ">
+      <Image src={"./logo.svg"} alt="" width={160} height={100} />
+
+      {isSignedIn ? (
+        <UserButton />
+      ) : (
+        <Link href={'/sign-in'}>
+        <Button variant="" className="bg-indigo-600 hover:bg-indigo-800">
+          Get Started
+        </Button>
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default Header;
---
+++ app/_components/Hero.jsx
@@ -0,0 +1,58 @@
+"use client"
+
+import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Hero = () => {
+  const { user, isSignedIn } = useUser();
+
+  return (
+    <section className="bg-gray-50 flex items-center flex-col">
+      <div className="mx-auto w-screen max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 lg:py-32">
+        <div className="mx-auto max-w-prose text-center">
+          <h1 className="text-4xl font-bold text-gray-900 sm:text-5xl">
+            Manage Your Expense
+            <br />
+            <strong className="text-indigo-600"> Control </strong>
+            Your Money
+          </h1>
+
+          <p className="mt-4 text-base text-pretty text-gray-700 sm:text-lg/relaxed">
+            Start Creating your budget and Save tn of Money.
+          </p>
+
+          {isSignedIn ? (
+            <div className="mt-4 flex justify-center gap-4 sm:mt-6">
+              <p
+                className="inline-block rounded border border-indigo-600 bg-indigo-600 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-indigo-700"
+              >
+                Continue
+              </p>
+            </div>
+          ) : (
+            <div className="mt-4 flex justify-center gap-4 sm:mt-6">
+              <Link
+                href={"/sign-in"}
+                className="inline-block rounded border border-indigo-600 bg-indigo-600 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-indigo-700"
+              >
+                Get Started
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+
+      <Image
+        src={"/dashboard.png"}
+        alt=""
+        width={1000}
+        height={700}
+        className="-mt-9 rounded-xl border-2"
+      />
+    </section>
+  );
+};
+
+export default Hero;
